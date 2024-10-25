60ccb9d95d3392449782d32e43558dcca6b2f610
codebash <codebash@asus-codebash.home>
2024-10-25 17:52:28 +0200

    ajout footer et apropos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { PrimeNgComponents } from './primeng/primeng.module';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { AproposComponent } from './components/apropos/apropos.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeNgComponents, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, PrimeNgComponents, HeaderComponent, HomeComponent, AproposComponent, GalleryComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
